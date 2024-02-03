39af756cba8e1323134f978ee8681ca7cd57a251
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2024-02-03 16:18:10 -0500

    navbar changes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,3 @@
-// Assuming "use client" is necessary at the top if you're specifically instructed to do so by Next.js documentation or error messages
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -10,7 +9,7 @@ import { HomeIcon, UsersIcon, SearchIcon, UserIcon } from "lucide-react";
 
 const NavBar = ({ atLogin, profilePicUrl }) => {
   const [activeNavLink, setActiveNavLink] = useState("");
-  const router = useRouter(); // This will not cause an error as it's used inside a component
+  const router = useRouter();
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -18,15 +17,12 @@ const NavBar = ({ atLogin, profilePicUrl }) => {
       setScrolled(window.pageYOffset > 0);
     };
 
-    // Attach scroll event listener
     window.addEventListener("scroll", handleScroll);
 
-    // Clean up the event listener on component unmount
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   useEffect(() => {
-    // Assuming you want to perform some actions based on the route, do it here
   }, [router.pathname]);
 
   if (atLogin) {
@@ -40,7 +36,6 @@ const NavBar = ({ atLogin, profilePicUrl }) => {
   return (
     <div className={`flex justify-between w-full items-center py-8 px-5 h-16 sticky top-0 z-50 transition-all duration-300 bg-inherit ${scrolled ? "shadow-md border-b border-gray-200 dark:border-gray-600" : ""}`}>
       <div className="flex gap-2">
-        {/* Placeholder for slider or any other content */}
         <HomeIcon className="w-6 h-6" />
         <UsersIcon className="w-6 h-6" />
       </div>
