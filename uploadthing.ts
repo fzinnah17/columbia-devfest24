fd218bfbb81f52487dbc802c7270cd05d8ab2420
Ricky <rzh4321@gmail.com>
2024-02-04 06:43:38 -0500

    removed types from uploadthing

--- uploadthing.ts
+++ uploadthing.ts
@@ -2,8 +2,6 @@ import {
     generateUploadButton,
     generateUploadDropzone,
   } from "@uploadthing/react";
-   
-  import type { OurFileRouter } from "@/app/api/uploadthing/core";
-   
-  export const UploadButton = generateUploadButton<OurFileRouter>();
-  export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+      
+  export const UploadButton = generateUploadButton();
+  export const UploadDropzone = generateUploadDropzone();
